8be70d22aab9406350e51ef14c58d0e0eb47b943
TATSUYA321 <naaag0210@yahoo.co.jp>
2021-02-18 16:10:46 +0000

    Infomationediting

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -12,4 +12,9 @@ class Public::CustomersController < ApplicationController
     @customer =Customer.find(params[:id])
   end
 
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
 end
