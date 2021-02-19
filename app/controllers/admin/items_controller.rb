a23a796c0ff6df375e6f825c00938bd3e1a995a7
syozo1201 <00syozo1201@gmail.com>
2021-02-19 04:19:31 +0000

    作業途中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,18 +1,17 @@
 class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
-    @genres = Genre.all
+    #@genres = Genre.all
   end
 
   def create
     @item = Item.new(item_params)
-    @item.save
+    @item.save!
     redirect_to admin_items_path
   end
 
   def index
      @items = Item.all
-
   end
 
   def show
@@ -32,7 +31,7 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :introduction, :price, :genre)
+    params.require(:item).permit(:name, :image, :introduction, :price, :is_active)
   end
 
 end
