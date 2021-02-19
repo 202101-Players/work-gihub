314b4e9111ac82421aea79845076a9f31f8b934b
TAKAHIRO <takahiro1109tm568@gmail.com>
2021-02-19 09:39:06 +0000

    admin-customer-unfinish

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,18 +1,26 @@
 class Admin::CustomersController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
+  
   def index
      @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
-  def destroy
+  def edit
+     @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to  admin_customer_path, success: '登録に成功しました'
   end
 
+ private
+
+  def genre_params
+    params.require(:customer).permit(:email, :last_name, :first_mame, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted)
+  end
 end
--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -11,7 +11,7 @@ class Admin::GenresController < ApplicationController
     
   end
   
-    
+  
   
   def edit
     @genre =  Genre.find(params[:id])
