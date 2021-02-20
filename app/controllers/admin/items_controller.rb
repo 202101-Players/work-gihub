5eba2faee1a7f3929156f1af0cd1a0a16798ebca
syozo1201 <00syozo1201@gmail.com>
2021-02-20 02:23:17 +0000

    管理側商品

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -17,11 +17,11 @@ class Admin::ItemsController < ApplicationController
   def show
     @item =  Item.find(params[:id])
   end
-  
+
   def edit
     @item =  Item.find(params[:id])
   end
-  
+
   def update
     item = Item.find(params[:id])
     item.update!(item_params)
